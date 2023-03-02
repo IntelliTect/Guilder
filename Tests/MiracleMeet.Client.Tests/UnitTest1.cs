49dec3600203206ed8bd05ae37ce2431bd9a8bbf
Kevin Bost <kitokeboo@gmail.com>
2023-03-01 22:15:19 -0800

    Adding build and publish

---
+++ Tests/MiracleMeet.Client.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MiracleMeet.Client.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tests/MiracleMeet.Server.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MiracleMeet.Server.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
