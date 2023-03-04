6c67577036f23872ae75b29e0ecd88717f2186f3
Mark Michaelis <mark@IntelliTect.com>
2023-03-03 20:36:14 -0800

    Fixed bad code :(

--- Guilder/Shared/MeetingClient.cs
+++ Guilder/Shared/MeetingClient.cs
@@ -16,7 +16,7 @@ namespace Guilder.Shared
         public async Task<IEnumerable<Meeting>> GetMeetingsForRoomId(string roomId)
         {
             var options = new JsonSerializerOptions(JsonSerializerDefaults.Web).ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
-            var response = await _http.GetFromJsonAsync<IEnumerable<Meeting>>($"room/{roomId}/meeting", options);
+            var response = await Http.GetFromJsonAsync<IEnumerable<Meeting>>($"room/{roomId}/meeting", options);
 
             return response ?? new List<Meeting>();
         }
