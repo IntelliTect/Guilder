0c3ae25d7e8be14c4f38430c20e6a06be1761b6c
Mark Michaelis <mark@IntelliTect.com>
2023-03-03 21:32:31 -0800

    Removed additional constructor from Meeting

--- Guilder/Shared/Meeting.cs
+++ Guilder/Shared/Meeting.cs
@@ -2,12 +2,10 @@
 
 namespace Guilder.Shared;
 
-public record class Meeting(string Name, Instant StartTimeInclusive, Instant EndTimeExclusive, string? Description)
+public record class Meeting(
+    string Name, Instant StartTimeInclusive, Instant EndTimeExclusive, string? Description = null)
 {
 
-    public Meeting(string roomId, Instant expectedStart, Instant expectedEnd) : this(
-        roomId, expectedStart, expectedEnd, null)
-    { }
 
 
 }
