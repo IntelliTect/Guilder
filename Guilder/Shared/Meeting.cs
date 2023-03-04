f5004fc88033e920b1f6ffccbf778c9139998e48
Artem Chetverikov <artemhetverikov@gmail.com>
2023-03-03 16:58:27 -0800

    Add Pocos

---
+++ Guilder/Shared/Meeting.cs
@@ -0,0 +1,17 @@
+﻿using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guilder.Shared
+{
+    public class Meeting
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Instant StartTimeInclusive { get; set; }
+        public Instant EndTimeExclusive { get; set; }
+    }
+}
