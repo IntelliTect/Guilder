2a25918f041ddc96672d5fa2a47e36bfff32def2
Kevin Bost <kitokeboo@gmail.com>
2023-03-03 17:35:21 -0800

    Removing WeatherForecast

--- Guilder/Shared/Meeting.cs
+++ Guilder/Shared/Meeting.cs
@@ -1,17 +1,11 @@
 ﻿using NodaTime;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Guilder.Shared
+namespace Guilder.Shared;
+
+public class Meeting
 {
-    public class Meeting
-    {
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public Instant StartTimeInclusive { get; set; } = Instant.MinValue;
-        public Instant EndTimeExclusive { get; set; } = Instant.MaxValue;
-    }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public Instant StartTimeInclusive { get; set; } = Instant.MinValue;
+    public Instant EndTimeExclusive { get; set; } = Instant.MaxValue;
 }
--- Guilder/Shared/Room.cs
+++ Guilder/Shared/Room.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Guilder.Shared;
 
-namespace Guilder.Shared
+public class Room
 {
-    public class Room
-    {
-        public List<Meeting> Meetings { get; set; } = new();
-    }
+    public List<Meeting> Meetings { get; set; } = new();
 }
