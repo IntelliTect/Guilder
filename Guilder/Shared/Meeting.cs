b29db6c7097c0d8c84663ecb5286e103995d7768
Mark Michaelis <mark@IntelliTect.com>
2023-03-03 20:24:29 -0800

    Convert classes to record classes

--- Guilder/Shared/Meeting.cs
+++ Guilder/Shared/Meeting.cs
@@ -2,10 +2,4 @@
 
 namespace Guilder.Shared;
 
-public class Meeting
-{
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public Instant StartTimeInclusive { get; set; } = Instant.MinValue;
-    public Instant EndTimeExclusive { get; set; } = Instant.MaxValue;
-}
+public record class Meeting(string Name, string Description, Instant StartTimeInclusive, Instant EndTimeExclusive);
--- Guilder/Shared/Room.cs
+++ Guilder/Shared/Room.cs
@@ -1,13 +1,3 @@
 ﻿namespace Guilder.Shared;
 
-public class Room
-{
-    public string Id { get; set; }
-    public string Name { get; set; }
-
-    public Room(string id, string name)
-    {
-        Id = id;
-        Name = name;
-    }
-}
+public record class Room(string Id, string Name);
